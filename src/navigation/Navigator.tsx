bc510dd04d43e9c9e80eddf3e03877a9b234bc79
Julien M <julien.marcesse@gmail.com>
2020-01-27 21:47:01 +0100

    implémentation de la nav bar avec couleurs

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,36 +1,58 @@
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import React from "react";
+
 import { createAppContainer } from "react-navigation";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "../screens/HomeScreen";
 import DashboardScreen from "../screens/DashBoardScreen";
 import EventsScreen from "../screens/EventsScreen";
+import ParametersScreen from "../screens/ParametersScreen";
 
 const Navigator = createBottomTabNavigator(
   {
     Home: {
       screen: HomeScreen,
       navigationOptions: {
-        tabBarLabel: "Accueil"
+        tabBarLabel: "Accueil",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-apps" color={tintColor} size={25} />
+        )
       }
     },
     DashBoard: {
       screen: DashboardScreen,
       navigationOptions: {
-        tabBarLabel: "Mon tableau de bord"
+        tabBarLabel: "DashBoard",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-home" color={tintColor} size={25} />
+        )
       }
     },
     Events: {
       screen: EventsScreen,
       navigationOptions: {
-        tabBarLabel: "Mes évènements"
+        tabBarLabel: "Mes évènements",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-create" color={tintColor} size={25} />
+        )
+      }
+    },
+    Params: {
+      screen: ParametersScreen,
+      navigationOptions: {
+        tabBarLabel: "Paramètres",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-options" color={tintColor} size={25} />
+        )
       }
     }
   },
   {
     tabBarOptions: {
       activeTintColor: "red",
-      inactiveTintColor: "grey"
+      inactiveTintColor: "grey",
+      showIcon: true
     }
   }
 );
