637d6e95a7da46630f408bf33d696056b642d83a
Julien M <julien.marcesse@gmail.com>
2020-03-15 12:44:23 +0100

    service and store for equiment

--- src/store/RootStore.ts
+++ src/store/RootStore.ts
@@ -2,18 +2,21 @@ import MemberStore from "./MemberStore";
 import TankStore from "./TankStore";
 import WaterTestStore from "./WaterTestStore";
 import AnimalStore from "./AnimalStore";
+import EquipmentStore from "./EquipmentStore";
 
 export class RootStore {
   memberStore: MemberStore;
   tankStore: TankStore;
   waterTestStore: WaterTestStore;
   animalStore: AnimalStore;
+  equipmentStore: EquipmentStore;
 
   constructor() {
     this.memberStore = new MemberStore(this);
     this.tankStore = new TankStore(this);
     this.waterTestStore = new WaterTestStore(this);
     this.animalStore = new AnimalStore(this);
+    this.equipmentStore = new EquipmentStore(this);
   }
 }
 
