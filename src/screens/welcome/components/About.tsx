8e0dd0753ec668ae1680b433336a27db8d542469
Julien M <julien.marcesse@gmail.com>
2020-12-10 23:02:20 +0100

    rework about

--- src/screens/welcome/components/About.tsx
+++ src/screens/welcome/components/About.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View } from "react-native";
 import {
   version as app_version,
   name as app_name,
 } from "../../../../package.json";
 
-import aboutImage from "../../../assets/about.png";
-import mailIcon from "../../../assets/icons/mail.png";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import email from "react-native-email";
 
@@ -25,40 +23,14 @@ export const About = () => {
         alignItems: "center",
       }}
     >
-      <Image
-        source={aboutImage}
-        style={{
-          width: 256,
-          height: 128,
-          borderRadius: 16,
-          margin: 8,
-        }}
-      />
       <Text style={{ textAlign: "center" }}>
         Application développée par Julien Marcesse
       </Text>
-
       <TouchableOpacity onPress={handleEmail}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-        >
-          <Image
-            source={mailIcon}
-            style={{ height: 32, width: 32, margin: 8 }}
-          />
-          <Text style={{ textAlign: "center", color: "blue" }}>
-            Pour toute question / suggestion :{"\n"}julien.marcesse@gmail.com
+        <Text style={{ textAlign: "center", color: "blue" }}>
+          Pour toute question / suggestion :{"\n"}julien.marcesse@gmail.com
           </Text>
-        </View>
       </TouchableOpacity>
-
-      <Text style={{ textAlign: "center" }}>Technologies utilisées</Text>
-      <Text>Front End : React Native</Text>
-      <Text>Back End : Spring Boot</Text>
       <Text style={{ color: "orange" }}>
         Version de l'appli : {app_name} {app_version}
       </Text>
