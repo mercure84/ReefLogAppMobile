55667901de2e7b2bba6796f609d95677b97b2d32
Julien M <julien.marcesse@gmail.com>
2021-03-28 00:42:14 +0100

    fix ssl port

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,3 @@
 //export const urlServer = "http://192.168.1.36:8080/";
 
-export const urlServer = "https://www.centropyge-bicolor.fr:8080/";
+export const urlServer = "https://www.centropyge-bicolor.fr:8443/";
