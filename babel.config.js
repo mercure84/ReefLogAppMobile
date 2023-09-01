386c2a5db444a86715c625874eae13fb56a6329b
Julien M <julien.marcesse@gmail.com>
2023-09-01 23:21:19 +0200

    first step

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {legacy: true},
+      'react-native-reanimated/plugin',
+    ],
+  ],
 };
